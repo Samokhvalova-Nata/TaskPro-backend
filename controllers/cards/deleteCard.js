5873118c60231c0c665b48c038e3a48e0fd97d53
kovalovaanastasiia <tokach26@gmail.com>
2023-08-30 12:53:18 +0300

    bug fixed

--- controllers/cards/deleteCard.js
+++ controllers/cards/deleteCard.js
@@ -1,13 +1,13 @@
-import { HttpError } from "../../helpers/index.js";
+import {HttpError} from "../../helpers/index.js";
 import Card from "../../models/card.js";
 
 const deleteCard = async (req, res) => {
-    const { id } = req.params;
+    const {id} = req.params;
     const result = await Card.findByIdAndRemove(id);
     if (!result) {
         throw HttpError(404, `Card with id=${id} not found`);
     }
-    res.status(200).json({ message: "Card deleted" });
+    res.status(200).json({ id, message: "Card deleted"});
 };
 
 export default deleteCard;
