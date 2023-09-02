ff144d31438848d42b48e3f3fc13928003bed331
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-09-02 13:42:28 +0300

    Update port email sending

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -6,7 +6,7 @@ const {UKR_NET_EMAIL, UKR_NET_PASSWORD} = process.env;
 
 const nodemailerConfig = {
     host: "smtp.ukr.net",
-    port: 2525,
+    port: 465,
     secure: true,
     auth: {
         user: UKR_NET_EMAIL,
